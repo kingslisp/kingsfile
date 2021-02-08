9b82c464505c19555ac5acb8f6969b982a401aed
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-02-08 07:10:50 +0100

    Initial commit

---
+++ sources/kingsfile_core.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define KINGSFILE_VERSION_MAJOR 0
+#define KINGSFILE_VERSION_MINOR 0
+#define KINGSFILE_VERSION_PATCH 1
+#define KINGSFILE_VERSION_EXTRA "-dev"
+#define KINGSFILE_VERSION_FULL  "0.0.1-dev"
+#define KINGSFILE_EXITCODE      0
+#define KINGSFILE_SKIP_NEXT     0
+
