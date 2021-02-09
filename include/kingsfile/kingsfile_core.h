9579599360dc33cacea700ecde3c2e041ecd3e92
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-02-09 10:37:42 +0100

    Added kingsfile core includes

--- include/kingsfile/kingsfile_core.h
+++ include/kingsfile/kingsfile_core.h
@@ -1,4 +1,15 @@
 #ifndef __KINGSFILE_CORE
 #define __KINGSFILE_CORE
 
+struct build_options {
+  char *project, *compiler, *output, *default_target;
+  char **sources, **includes, **libraries, **library_dirs, **compiler_options;
+  int sources_count, includes_count, libraries_count, library_dirs_count, compiler_options_count;
+};
+
+typedef struct build_options BuildOptions;
+char *kingsfile_read_file(char *);
+BuildOptions kingsfile_build_options(char *, char *);
+void kingsfile_build_project(BuildOptions *);
+
 #endif
